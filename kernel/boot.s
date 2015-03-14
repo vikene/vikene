dce7ec8a95d8f04452e7c64dfc09cc5ddeb98899
Vikene <vigneashsundar@live.com>
2015-03-14 10:55:22 +0530

    Working BootLoader

---
+++ kernel/boot.s
@@ -0,0 +1,44 @@
+; 
+; Vikene Kernel (c) Vigneash Sundar 2015
+; Purly educational purpose 
+; Commercial license is void 
+; Multiboot Headers for GRUB conf 
+;
+
+MBOOT_PAGE_ALIGN equ 1<<0
+MBOOT_MEM_INFO	equ 1<<1
+MBOOT_HEADER_MAGIC equ 0x1BADB002 ;MULTI BOOT MAGIC VALUE
+
+MBOOT_HEADER_FLAGS equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO
+MBOOT_CHECKSUM equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
+
+[BITS 32]
+
+[GLOBAL mboot]
+[EXTERN code]
+[EXTERN bss]
+[EXTERN end]
+
+mboot:
+	dd MBOOT_HEADER_MAGIC
+
+	dd MBOOT_HEADER_FLAGS
+	dd MBOOT_CHECKSUM
+
+	dd mboot
+	dd code
+	dd bss
+	dd end
+	dd start
+
+[GLOBAL start]
+[EXTERN main]
+
+start:
+
+	push ebx
+
+cli
+call main
+jmp $
+
